2e7d737d45da463e3d0d76469b49308fa2a03a61
Martyna Plomecka <martyna.plomecka@uzh.ch>
2021-10-26 15:46:09 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-	Striped by HTML5 UP
+	Tessellate by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -7,16 +7,14 @@
 (function($) {
 
 	var	$window = $(window),
-		$body = $('body'),
-		$document = $(document);
+		$body = $('body');
 
 	// Breakpoints.
 		breakpoints({
-			desktop:   [ '737px',   null     ],
-			wide:      [ '1201px',  null     ],
-			narrow:    [ '737px',   '1200px' ],
-			narrower:  [ '737px',   '1000px' ],
-			mobile:    [ null,      '736px'  ]
+			wide:    [ '1281px',  '1680px' ],
+			normal:  [ '1001px',  '1280px' ],
+			narrow:  [ '737px',   '1000px' ],
+			mobile:  [ null,      '736px'  ]
 		});
 
 	// Play initial animations on page load.
@@ -26,45 +24,7 @@
 			}, 100);
 		});
 
-	// Nav.
-
-		// Height hack.
-		/*
-			var $sc = $('#sidebar, #content'), tid;
-
-			$window
-				.on('resize', function() {
-					window.clearTimeout(tid);
-					tid = window.setTimeout(function() {
-						$sc.css('min-height', $document.height());
-					}, 100);
-				})
-				.on('load', function() {
-					$window.trigger('resize');
-				})
-				.trigger('resize');
-		*/
-
-		// Title Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#sidebar" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Sidebar
-			$('#sidebar')
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'sidebar-visible'
-				});
+	// Scrolly.
+		$('.scrolly').scrolly();
 
 })(jQuery);
